e951ad48d7d93f2058fa62f9d1d5764b90f492fd
Leandro Martinez <leandro.martinez@thelucky.app>
2024-02-17 15:54:34 -0300

    add nodemon library

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,5 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port} 🚀`);
 });
